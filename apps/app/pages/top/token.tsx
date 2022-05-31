74b3531989555599098aad111050aa11f70368ba
Anton O <mavarazy@gmail.com>
2022-05-31 15:56:48 +0300

    Adding heading

--- apps/app/pages/top/token.tsx
+++ apps/app/pages/top/token.tsx
@@ -20,6 +20,17 @@ const TopTokensPage: NextPage<{
 }> = ({ tokens }) => (
   <main className="flex flex-1 flex-col">
     <div className="flex flex-1 m-2 sm:m-4 justify-center items-center">
+      <div className="max-w-5xl flex flex-col flex-1 self-start">
+        <h1 className="text-3xl mt-3 tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          <span className="xl:inline ml-2">Tokens</span>{' '}
+          <span className="text-brand xl:inline">by worth</span>
+        </h1>
+        <h1 className="text-xl ml-3 sm:ml-5 mb-3 tracking-tight font-extrabold text-brand">
+          In top 500 wallets on Solana
+        </h1>
+      </div>
+    </div>
+    <div className="flex flex-1 m-2 sm:m-4 justify-center items-center">
       <div className="max-w-5xl flex flex-col flex-1">
         <div
           role="list"
