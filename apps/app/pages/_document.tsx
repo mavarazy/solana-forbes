07024731d1b26598809b76e43b7488af7b91f527
Anton O <mavarazy@gmail.com>
2022-06-02 16:32:59 +0300

    More fixes

--- apps/app/pages/_document.tsx
+++ apps/app/pages/_document.tsx
@@ -3,14 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class DropioDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            cross-origin="true"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
             rel="stylesheet"
